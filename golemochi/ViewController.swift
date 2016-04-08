325fcf48e5a52b2af84f16a89a56c89c24589f9d
Maslor <gabriel.freire@tecnico.ulisboa.pt>
2016-04-08 14:30:27 +0100

    Initial Commit

---
+++ golemochi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  golemochi
+//
+//  Created by Gabriel Freire on 08/04/16.
+//  Copyright © 2016 maslor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
