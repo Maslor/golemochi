3305358a2b1a71f7d7f891357cb6d69c650fb7ea
Maslor <gabriel.freire@tecnico.ulisboa.pt>
2016-04-20 15:04:07 +0100

    implemented functionality and added sounds

--- golemochi/ViewController.swift
+++ golemochi/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -21,25 +22,73 @@ class ViewController: UIViewController {
     let OPAQUE : CGFloat = 1.0
     let MAX_PENALTIES = 3
     
+    var happy = false
     var penalties = 0
     var timer : NSTimer!
+    var currentItem: UInt32 = 0
+    
+    var mPlayer: AVAudioPlayer!
+    var sfxFood: AVAudioPlayer!
+    var sfxSkull: AVAudioPlayer!
+    var sfxHeart: AVAudioPlayer!
+    var sfxDeath: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         skull1.alpha = DIM_ALPHA
         skull2.alpha = DIM_ALPHA
         skull3.alpha = DIM_ALPHA
+        foodImage.alpha = DIM_ALPHA
+        heartImage.alpha = DIM_ALPHA
         
         foodImage.targetElement = golemImage
         heartImage.targetElement = golemImage
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector:#selector(ViewController.itemDroppedOnCharacter(_:)), name: "onTargetDropped", object: nil)
         
+        do {
+            let resourcePath = NSBundle.mainBundle().pathForResource("cave-music", ofType: "mp3")!
+            let url = NSURL(fileURLWithPath: resourcePath)
+            try mPlayer = AVAudioPlayer(contentsOfURL: url)
+            
+            try sfxFood =  AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bite", ofType: "wav")!))
+            
+            try sfxDeath =  AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("death", ofType: "wav")!))
+            
+            try sfxHeart =  AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("heart", ofType: "wav")!))
+            
+            try sfxSkull =  AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("skull", ofType: "wav")!))
+            
+            mPlayer.prepareToPlay()
+            mPlayer.play()
+            
+            sfxFood.prepareToPlay()
+            sfxDeath.prepareToPlay()
+            sfxHeart.prepareToPlay()
+            sfxSkull.prepareToPlay()
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
         startTimer()    
     }
     
     func itemDroppedOnCharacter(notif: AnyObject) {
-        print("Item Dropped on Character")
+        happy = true
+        startTimer()
+        
+        foodImage.alpha = DIM_ALPHA
+        foodImage.userInteractionEnabled = false
+        heartImage.alpha = DIM_ALPHA
+        heartImage.userInteractionEnabled = false
+        
+        if currentItem == 0 {
+            sfxHeart.play()
+        } else {
+            sfxFood.play()
+        }
     }
     
     func startTimer() {
@@ -51,29 +100,50 @@ class ViewController: UIViewController {
     }
     
     func changeGameState() {
-        penalties += 1
-        if penalties == 1 {
-            skull1.alpha = OPAQUE
-            skull2.alpha = DIM_ALPHA
-        } else if penalties == 2 {
-            skull2.alpha = OPAQUE
-            skull3.alpha = DIM_ALPHA
-        } else if penalties >= 3 {
-            skull3.alpha = OPAQUE
-        } else {
-            skull1.alpha = DIM_ALPHA
-            skull2.alpha = DIM_ALPHA
-            skull3.alpha = DIM_ALPHA
+        if !happy {
+            penalties += 1
+            sfxSkull.play()
+            if penalties == 1 {
+                skull1.alpha = OPAQUE
+                skull2.alpha = DIM_ALPHA
+            } else if penalties == 2 {
+                skull2.alpha = OPAQUE
+                skull3.alpha = DIM_ALPHA
+            } else if penalties >= 3 {
+                skull3.alpha = OPAQUE
+            } else {
+                skull1.alpha = DIM_ALPHA
+                skull2.alpha = DIM_ALPHA
+                skull3.alpha = DIM_ALPHA
+            }
+        
+            if penalties >= MAX_PENALTIES {
+                gameOver()
+            }
         }
         
-        if penalties >= MAX_PENALTIES {
-            gameOver()
+        let rand = arc4random_uniform(2)
+        
+        if rand == 0 {
+            foodImage.alpha = DIM_ALPHA
+            heartImage.alpha = OPAQUE
+            foodImage.userInteractionEnabled = false
+            heartImage.userInteractionEnabled = true
+        } else {
+            heartImage.alpha = DIM_ALPHA
+            foodImage.alpha = OPAQUE
+            foodImage.userInteractionEnabled = true
+            heartImage.userInteractionEnabled = false
         }
+        
+        currentItem = rand
+        happy = false
     }
     
     func gameOver() {
         timer.invalidate()
         golemImage.playDeathAnimation()
+        sfxDeath.play()
     }
 }
 
